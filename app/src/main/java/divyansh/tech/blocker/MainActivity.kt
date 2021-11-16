a51f77ac210bead32821a8dc15c0fce4d8ac08c9
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2021-11-16 21:50:46 +0530

    added incoming call receiver

--- app/src/main/java/divyansh/tech/blocker/MainActivity.kt
+++ app/src/main/java/divyansh/tech/blocker/MainActivity.kt
@@ -1,13 +1,27 @@
 package divyansh.tech.blocker
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telecom.Call
+import android.telephony.TelephonyManager
 import dagger.hilt.android.AndroidEntryPoint
+import divyansh.tech.blocker.common.CallReceiver
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val recv = CallReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        registerReceiver(recv, IntentFilter(TelephonyManager.ACTION_PHONE_STATE_CHANGED))
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(recv)
     }
 }
