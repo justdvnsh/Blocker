c7a8e9edf92991b6e789dd86518b6072723cc604
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2021-11-17 16:10:12 +0530

    added a notification showing feature to show when a call is blocked

--- app/src/main/java/divyansh/tech/blocker/common/IncomingCallReceiver.kt
+++ app/src/main/java/divyansh/tech/blocker/common/IncomingCallReceiver.kt
@@ -1,27 +1,31 @@
 package divyansh.tech.blocker.common
 
 import android.Manifest
+import android.R
 import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.telecom.TelecomManager
-import android.telephony.PhoneNumberUtils
 import android.telephony.TelephonyManager
 import android.widget.Toast
-import androidx.annotation.CallSuper
 import androidx.core.app.ActivityCompat
-import java.lang.reflect.Method
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat.getSystemService
 import com.android.internal.telephony.ITelephony
-import dagger.assisted.AssistedFactory
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.qualifiers.ApplicationContext
+import divyansh.tech.blocker.MainActivity
 import divyansh.tech.blocker.common.database.ContactDao
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import java.lang.reflect.Method
 import javax.inject.Inject
 
 
@@ -30,6 +34,10 @@ class IncomingCallReceiver: BroadcastReceiver() {
 
     @Inject
     lateinit var dao: ContactDao
+
+    @Inject
+    @ApplicationContext
+    lateinit var context: Context
 
     private lateinit var blockedNumbers: List<ContactModel>
 
@@ -63,11 +71,14 @@ class IncomingCallReceiver: BroadcastReceiver() {
                 ) {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
                         Toast.makeText(context, "INSIDE TELECOM MANAGER", Toast.LENGTH_SHORT).show()
-                        runBlocking {
+                        GlobalScope.launch(Dispatchers.IO) {
                             blockedNumbers = dao.getAllBlockedContacts()
                             blockedNumbers.forEach {
-                                Toast.makeText(context, "$number and ${it.phone}", Toast.LENGTH_SHORT).show()
-                                if (number.equals(it.phone)) telecomManager.endCall()
+                                if (number.equals(it.phone)) {
+                                    telecomManager.endCall()
+                                    createNotificationChannel()
+                                    addNotification(it.name, context)
+                                }
                             }
                         }
                     }
@@ -85,5 +96,43 @@ class IncomingCallReceiver: BroadcastReceiver() {
         } catch (e: Exception) {
             e.printStackTrace()
         }
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "blocker"
+            val descriptionText = "Blocked contacts calling notification"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("blocker", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun addNotification(name: String, context: Context) {
+        val builder: NotificationCompat.Builder = NotificationCompat.Builder(context, "blocker")
+            .setSmallIcon(R.drawable.gallery_thumb) //set icon for notification
+            .setContentTitle("Blocker Blocked a contact") //set title of notification
+            .setContentText("Blocker blocked a call from $name") //this is notification message
+            .setAutoCancel(true) // makes auto cancel of notification
+            .setPriority(NotificationCompat.PRIORITY_HIGH) //set priority of notification
+        val notificationIntent = Intent(context, MainActivity::class.java)
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        //notification message will get at NotificationView
+        notificationIntent.putExtra("message", "This is a notification message")
+        val pendingIntent = PendingIntent.getActivity(
+            context, 0, notificationIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        builder.setContentIntent(pendingIntent)
+
+        // Add as notification
+        val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.notify(0, builder.build())
     }
 }
